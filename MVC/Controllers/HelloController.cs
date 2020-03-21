f765439c8dd493b9b149069d548e0ab09858cbea
zhangyongfeng <535069215@qq.com>
2020-03-21 18:39:13 +0800

    init

---
+++ MVC/Controllers/HelloController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class HelloController : Controller
+    {
+        //GET: Hello
+        public ActionResult Index(string name, int age = 0)
+        {
+            ViewBag.name = name;
+            ViewBag.age = age;
+            return View();
+        }
+        //public string Index(string name, int age = 0, int ID = 1)
+        //{
+        //    //return "hello word" + name + "age = " + age.ToString();
+        //    return HttpUtility.HtmlEncode("hello word" + name + "age = " + age.ToString() + "ID="+ID.ToString());
+        //}
+    }
+}
